ad75fa8fc592281c399bf2f7676efd0188178e2f
Jake20180515 <39300576+Jake20180515@users.noreply.github.com>
2018-06-08 01:34:38 +0900

    d
    dd

--- Assets/Scripts/enemy.cs
+++ Assets/Scripts/enemy.cs
@@ -2,30 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemy : MonoBehaviour {
+public class enemy : MonoBehaviour
+{
 
     public Vector3 pos;
-    public float speed;
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float speed = 18f;
+
     private void Awake()
     {
         pos = new Vector3(130f, 0, 0);
-        speed = 0.25f; 
     }
 
-    void move()
+    private void FixedUpdate()
     {
-        float xMove = speed;
-        transform.Translate(-xMove, 0, 0);
-        if (transform.position.x < -130f)
+        this.transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        if (this.pos.x == -30f)
             Destroy(this.gameObject);
-    }
-	
-	// Update is called once per frame
-	void Update () {
-        move();
     }
 }
