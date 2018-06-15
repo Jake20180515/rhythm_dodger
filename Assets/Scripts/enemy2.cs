476198d41727feacc6dfc790ff276037fe928103
jy0918 <40190209+jy0918@users.noreply.github.com>
2018-06-15 11:51:25 +0900

    장애물 추가
    spawnManger 스크립트 + 객체 수정
    enemy 1,2 스크립트 추가
    enemy_prefab1,2 추가

---
+++ Assets/Scripts/enemy2.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy2 : MonoBehaviour
+{
+
+    public Vector3 pos;
+    public float timer;
+    public float speed = 18f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    private void Awake()
+    {
+       // pos = new Vector3(130f, 15, 0);
+     //   transform.position = pos;
+    }
+
+    void move()
+    {
+
+        transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+
+        timer += Time.deltaTime;
+
+        if (timer >= 1)
+        {
+            transform.localScale = new Vector3(1.399413f, 3.509718f, 0);
+            timer = 0;
+        }
+
+        else if (timer >= 0.5)
+        {
+            transform.localScale = new Vector3(1.399413f, 15f, 0);
+            //timer = 0;
+        }
+
+        if (transform.position.x < -130f)
+            Destroy(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move();
+    }
+}
--- Assets/Scripts/spawnManeger.cs
+++ Assets/Scripts/spawnManeger.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class spawnManeger : MonoBehaviour {
     public bool enableSpawn = false;
     public GameObject Enemy;
+    public GameObject Enemy1;
+    public GameObject Enemy2;
     void SpawnEnemy()
     {
         if (enableSpawn)
@@ -12,16 +14,34 @@ public class spawnManeger : MonoBehaviour {
             GameObject enemy = (GameObject)Instantiate(Enemy, new Vector3(50f, 4.6f, 0f), Quaternion.identity);
         }
     }
-	// Use this for initialization
-	void Start () {
+
+    void SpawnEnemy1()
+    {
+        if (enableSpawn)
+        {
+            GameObject enemy1 = (GameObject)Instantiate(Enemy1, new Vector3(50f, 4.6f, 0f), Quaternion.identity);
+        }
+    }
+
+    void SpawnEnemy2()
+    {
+        if (enableSpawn)
+        {
+            GameObject enemy2 = (GameObject)Instantiate(Enemy2, new Vector3(50f, 7f, 0f), Quaternion.identity);
+        }
+    }
+
+
+    // Use this for initialization
+    void Start () {
         Invoke("SpawnEnemy", 4.1f);
-        Invoke("SpawnEnemy", 5.4f);
-        Invoke("SpawnEnemy", 6.8f);
+        Invoke("SpawnEnemy1", 5.4f);
+        Invoke("SpawnEnemy2", 6.8f);
         Invoke("SpawnEnemy", 8.3f);
-        Invoke("SpawnEnemy", 9.6f);
-        Invoke("SpawnEnemy", 11f);
-        Invoke("SpawnEnemy", 12.4f);
-        Invoke("SpawnEnemy", 15.1f);
+        Invoke("SpawnEnemy2", 9.6f);
+        Invoke("SpawnEnemy1", 11f);
+        Invoke("SpawnEnemy1", 12.4f);
+        Invoke("SpawnEnemy2", 15.1f);
         Invoke("SpawnEnemy", 16.4f);
         Invoke("SpawnEnemy", 17.9f);
         Invoke("SpawnEnemy", 19.3f);
